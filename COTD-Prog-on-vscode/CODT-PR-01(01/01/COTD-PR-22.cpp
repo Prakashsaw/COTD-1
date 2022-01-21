f8bf6bc091ea101d7840ac1a8f8e35c4282e212b
Prakash Saw <prakash1999saw@gmail.com>
2022-01-22 01:28:29 +0530

    modified pr-22

--- COTD-Prog-on-vscode/CODT-PR-01(01/01/COTD-PR-22.cpp
+++ COTD-Prog-on-vscode/CODT-PR-01(01/01/COTD-PR-22.cpp
@@ -14,3 +14,20 @@ public:
         return  ~n & mask;
     }
 };
+//second approach with 100% faster than other by using XOR operator
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0)
+            return 1;
+        int mask = 0;
+        int num = n;
+        while(num != 0)
+        {
+            mask = mask << 1;
+            mask = mask | 1;
+            num = num >> 1;
+        }
+        return  ~n ^ ~mask;
+    }
+};
